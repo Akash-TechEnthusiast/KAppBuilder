2f57b8b1c336cbcdb9f448c19cd7c99bb69783da
Husen <husenaiah.gandham@kagamierp.com>
2023-08-22 12:18:23 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+// import Sidebar from "../../components/sidebar/Sidebar";
+import CircularProgress from "@mui/material/CircularProgress";
+
+import "./home.scss";
+
+import Navbar from "../../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="homecontainer">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
