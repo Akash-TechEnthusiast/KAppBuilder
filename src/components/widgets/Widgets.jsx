127b85d70990d3164f7470a0b702becd7db54865
Husen <husenaiah.gandham@kagamierp.com>
2023-08-22 18:31:33 +0530

    adding boxshadow changes

---
+++ src/components/widgets/Widgets.jsx
@@ -0,0 +1,15 @@
+import "./widgets.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+
+const Widgets = () => {
+  return (
+    <div className="widget">
+      <div className="left"></div>
+
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Widgets;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,12 +4,22 @@ import CircularProgress from "@mui/material/CircularProgress";
 import "./home.scss";
 
 import Navbar from "../../components/navbar/Navbar";
+import Widgets from "../../components/widgets/Widgets";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="homecontainer">
         <Navbar />
+        <div className="widgets">
+          <Widgets />
+          <Widgets />
+          <Widgets />
+          <Widgets />
+          <Widgets />
+          <Widgets />
+          <Widgets />
+        </div>
       </div>
     </div>
   );
