fc51345322838467e053f04811713730a725bf60
Husen <husenaiah.gandham@kagamierp.com>
2023-08-24 18:48:44 +0530

    welcome page changes

--- src/pages/welcomepage/Welcomepage.jsx
+++ src/pages/welcomepage/Welcomepage.jsx
@@ -2,32 +2,22 @@ import React from "react";
 
 import welcomepage from "../../images/new13.jpg";
 import "./welcomepage.scss";
+import { IconButton } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const WelcomePage = () => {
-  const widgetview2 = {
-    // position: "relative",
-    width: "100%", // Set the desired container width
-    height: "100%", // Set the desired container height
-    border: "1px solid #ccc",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    //width: '300px',           // Set the desired width
-    //height: '200px',          // Set the desired height
-    //backgroundColor: "lightgray",
-  };
-
-  const widgetview = {
-    maxWidth: "100%", // Fit the image within the container width
-    maxHeight: "100%", // Ensure the image doesn't exceed the container height
-    padding: "10px",
-  };
-
   return (
-    <div style={widgetview2}>
-      <img src={welcomepage} alt="Image" style={widgetview} />
+    <div className="image-container">
+      <div className="image-wrapper">
+        <img src={welcomepage} alt="Image" />
+        <IconButton className="icon-button">
+          <ArrowForwardIosIcon />
+        </IconButton>
+      </div>
     </div>
   );
 };
 
 export default WelcomePage;
+
